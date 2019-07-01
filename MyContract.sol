1db4f225958e5b21b3502c64cce671b7a6efca1e
PhatNguyen <phatnt.uit@gmail.com>
2019-07-01 23:37:54 +0700

    Update MyContract.sol

--- MyContract.sol
+++ MyContract.sol
@@ -3,8 +3,27 @@ pragma solidity ^0.5.8;
 contract MyContract {
     string value;
     
+    // expose publicValue can be called outside of the contract
+    string public publicValue = "publicValue";
+    
+    // declare constant property
+    string public constant constantValue = "constantValue";
+    
+    // declare bool property
+    bool public myBool = true;
+    
+    // declare integer property
+    int public myInt = -1;
+    uint public myUint = 1;
+    
+    // declare enum
+    enum State { Waiting, Ready, Active};
+    
+    State public state;
+    
     constructor() public {
         value = "myValue";
+        state = State.Waiting;
     }
     
     function getValue() public view returns(string memory) {
@@ -13,5 +32,13 @@ contract MyContract {
     
     function setValue(string memory _value) public {
         value = _value;
+    }
+    
+    function activate() public {
+        state = State.Active;
+    }
+    
+    function checkIsActive() public view returns(bool) {
+        return state == State.Active;
     }
 }
